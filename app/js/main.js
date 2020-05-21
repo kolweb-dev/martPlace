9a514ce3de89547e598a2c925536ccb55273cc20
kolweb-dev <kolya.tkachukki@gmail.com>
2020-05-22 01:35:20 +0300

    added sections followers and popular

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,19 @@ $(function () {
 
 
     });
+    $('.followers-inner').slick({
+        nextArrow: '<button class="followers-btn followers-prev"><img src="images/icons/weekly-left.png" alt="next arrow"></button> ',
+        prevArrow: '<button class="followers-btn followers-next"><img src="images/icons/weekly-right.png" alt="prev arrow"></button> ',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+
+
+    });
+
+
     var mixer = mixitup('.products__inner-box');
 
 });
