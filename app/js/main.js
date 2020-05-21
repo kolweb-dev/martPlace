062c1b4f20d63f31b9838ab7731ca45868cfc268
kolweb-dev <kolya.tkachukki@gmail.com>
2020-05-21 17:48:02 +0300

    added products new and weekly

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
+    $('.weekly__inner').slick({
+        nextArrow: '<button class="weekly-btn weekly-prev"><img src="images/icons/weekly-left.png" alt="next arrow"></button> ',
+        prevArrow: '<button class="weekly-btn weekly-next"><img src="images/icons/weekly-right.png" alt="prev arrow"></button> ',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        fade: true,
+        cssEase: 'linear',
+
+
+    });
+    var mixer = mixitup('.products__inner-box');
 
 });
