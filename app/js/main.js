bd9c26ee3bce4283823c9894e6e245f3e8e28054
kolweb-dev <kolya.tkachukki@gmail.com>
2020-05-29 17:00:00 +0300

    added category

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function () {
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 330,
+        from: 30,
+        to: 300,
+        prefix: "$"
+
+    });
+
+
     $('.weekly__inner').slick({
         nextArrow: '<button class="weekly-btn weekly-prev"><img src="images/icons/weekly-left.png" alt="next arrow"></button> ',
         prevArrow: '<button class="weekly-btn weekly-next"><img src="images/icons/weekly-right.png" alt="prev arrow"></button> ',
@@ -29,8 +40,19 @@ $(function () {
         readOnly: true
     });
 
+    $('.icon-th-list').on('click', function () {
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th').removeClass('active');
+    });
+    $('.icon-th').on('click', function () {
+        $('.product__item').removeClass('list');
+        $('.icon-th').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
 
     var mixer = mixitup('.products__inner-box');
+
 
 
 });
